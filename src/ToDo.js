5a71dbf181afec060f1f4d0e844d0735f8e095db
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-26 09:52:19 +0300

    sveiki!

---
+++ src/ToDo.js
@@ -0,0 +1,15 @@
+import App from "./App";
+
+function ToDo(){
+    
+    return (
+        <>
+        <p>userId: 1</p> 
+    <p>id:1</p>
+    <p>title:"delecatus aut autem</p>
+    <input type = "checkbox"></input>
+        </>
+    )
+}
+
+export default ToDo;
